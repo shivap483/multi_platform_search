f3fb805dda38e37eda93ec340009ad396a73bee8
shiva <shivap483@gmail.com>
2023-06-26 16:01:04 +0530

    service layer implementation

--- src/app/config/app.config.ts
+++ src/app/config/app.config.ts
@@ -1,3 +1,10 @@
+const dotenv = require('dotenv');
 export const appConfig = {
-    port: 3000
+    port: process.env.PORT,
+    platforms: [
+        { name: 'amazon', url: `${process.env.AMAZON_BASE_URI}/search}`, queryKeyword:'q' },
+        { name: 'flipkart', url: `${process.env.AMAZON_BASE_URI}/search}`, queryKeyword:'keyword' },
+        { name: 'croma', url: `${process.env.AMAZON_BASE_URI}/search}`, queryKeyword:'key' },
+        { name: 'reliance_digital', url: `${process.env.AMAZON_BASE_URI}/search}`, queryKeyword:'q' }
+    ],
 }
--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -1,5 +1,40 @@
-const search = async (requestBody: any) => {
+import axios from 'axios';
+import { appConfig } from '../config/app.config';
 
+const search = async (query: any) => {
+    const platforms = appConfig.platforms;
+
+  // run serch queries concurrently
+  const searchPromises = platforms.map(async (platform) => {
+    const searchUrl = `${platform.url}?${platform.queryKeyword}=${query}`
+
+    try {
+      const response = await axios.get(searchUrl);
+      return {
+        platform: platform.name,
+        results: response.data.results
+      };
+    } catch (error) {
+      console.error(`Error searching ${platform.name}:`, error);
+      return {
+        platform: platform.name,
+        results: []
+      };
+    }
+  });
+
+  // wait for all platforms to resolve
+  const searchResults = await Promise.all(searchPromises);
+
+  const response = {
+    results: {}
+  };
+
+  searchResults.forEach((result) => {
+    response.results[result.platform] = result.results;
+  });
+
+  return searchResults;
 }
 
 export default {
