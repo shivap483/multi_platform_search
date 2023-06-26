ea36ecdc5de7278ee3bbf9bd1ee7e76d5107640e
shiva <shivap483@gmail.com>
2023-06-26 16:01:52 +0530

    prettify search service file

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -5,36 +5,36 @@ const search = async (query: any) => {
     const platforms = appConfig.platforms;
 
   // run serch queries concurrently
-  const searchPromises = platforms.map(async (platform) => {
-    const searchUrl = `${platform.url}?${platform.queryKeyword}=${query}`
+    const searchPromises = platforms.map(async (platform) => {
+        const searchUrl = `${platform.url}?${platform.queryKeyword}=${query}`
 
-    try {
-      const response = await axios.get(searchUrl);
-      return {
-        platform: platform.name,
-        results: response.data.results
-      };
-    } catch (error) {
-      console.error(`Error searching ${platform.name}:`, error);
-      return {
-        platform: platform.name,
-        results: []
-      };
-    }
-  });
+        try {
+            const response = await axios.get(searchUrl);
+            return {
+                platform: platform.name,
+                results: response.data.results
+            };
+            } catch (error) {
+            console.error(`Error searching ${platform.name}:`, error);
+            return {
+                platform: platform.name,
+                results: []
+            };
+        }
+    });
 
-  // wait for all platforms to resolve
-  const searchResults = await Promise.all(searchPromises);
+    // wait for all platforms to resolve
+    const searchResults = await Promise.all(searchPromises);
 
-  const response = {
-    results: {}
-  };
+    const response = {
+        results: {}
+    };
 
-  searchResults.forEach((result) => {
-    response.results[result.platform] = result.results;
-  });
+    searchResults.forEach((result) => {
+        response.results[result.platform] = result.results;
+    });
 
-  return searchResults;
+    return searchResults;
 }
 
 export default {
